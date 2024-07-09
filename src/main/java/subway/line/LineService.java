c451b18609ec7fab516cf9a239f1cf5ed298f1e0
dharana <dharana7723@gmail.com>
2024-07-10 03:49:44 +0900

    feat: line 엔티티 station OneToOne 적용으로 createLlne 메소드 station 가져오도록 변경 후 적용

--- src/main/java/subway/line/LineService.java
+++ src/main/java/subway/line/LineService.java
@@ -1,17 +1,40 @@
 package subway.line;
 
 import org.springframework.stereotype.Service;
+import subway.Station;
+import subway.StationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LineService {
 
   private final LineRepository lineRepository;
 
-  public LineService(LineRepository lineRepository) {
+  private final StationRepository stationRepository;
+
+  public LineService(LineRepository lineRepository, StationRepository stationRepository) {
     this.lineRepository = lineRepository;
+    this.stationRepository = stationRepository;
   }
 
   public void createLine(LineCreateRequest lineCreateRequest){
-    lineRepository.save(lineCreateRequest.toLine());
+    Station upStation = stationRepository.findById(lineCreateRequest.getUpStationId()).orElseThrow(RuntimeException::new);
+    Station downStation = stationRepository.findById(lineCreateRequest.getDownStationId()).orElseThrow(RuntimeException::new);
+    lineRepository.save(lineCreateRequest.toLine(upStation, downStation));
+  }
+
+  public List<LineResponse> getLines(){
+    List<Line> lines = lineRepository.findAll();
+    List<LineResponse> lineResponses = new ArrayList<>();
+    for (Line line : lines) {
+      List<Station> stations = new ArrayList<>();
+      stations.add(stationRepository.findById(line.getUpStation().getId()).orElseThrow(RuntimeException::new));
+      stations.add(stationRepository.findById(line.getDownStation().getId()).orElseThrow(RuntimeException::new));
+
+      lineResponses.add(new LineResponse(null, line.getName(), line.getColor(), stations));
+    }
+    return lineResponses;
   }
 }
