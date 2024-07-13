7aab29b9d7abe268c0bea80cfbd26420980f8554
dharana <dharana7723@gmail.com>
2024-07-13 14:31:19 +0900

    chore: 라인 응답 지하철 역 Station엔티티에서 StationResponse 형태로 변경

--- src/main/java/subway/line/LineResponse.java
+++ src/main/java/subway/line/LineResponse.java
@@ -1,6 +1,7 @@
 package subway.line;
 
 import subway.Station;
+import subway.StationResponse;
 
 import java.util.List;
 
@@ -12,12 +13,12 @@ public class LineResponse {
 
   private String color;
 
-  private List<Station> stations;
+  private List<StationResponse> stations;
 
   public LineResponse() {
   }
 
-  public LineResponse(Long id, String name, String color, List<Station> stations) {
+  public LineResponse(Long id, String name, String color, List<StationResponse> stations) {
     this.id = id;
     this.name = name;
     this.color = color;
@@ -36,11 +37,13 @@ public class LineResponse {
     return color;
   }
 
-  public List<Station> getStations(){
+  public List<StationResponse> getStations(){
     return stations;
   }
 
   public static LineResponse from(Line line) {
-    return new LineResponse(line.getId(), line.getName(), line.getColor(), List.of(line.getUpStation(), line.getDownStation()));
+    StationResponse upStation = StationResponse.from(line.getUpStation());
+    StationResponse downStation = StationResponse.from(line.getDownStation());
+    return new LineResponse(line.getId(), line.getName(), line.getColor(), List.of(upStation, downStation));
   }
 }
