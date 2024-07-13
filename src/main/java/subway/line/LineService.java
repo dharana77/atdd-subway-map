8ffec79e6b1ca4e6b6324e02136ccb79cf24cc5e
dharana <dharana7723@gmail.com>
2024-07-13 16:55:37 +0900

    feat: 예외 처리 추가 및 노선 아이디 값으로 찾기 메소드 분리

--- src/main/java/subway/line/LineService.java
+++ src/main/java/subway/line/LineService.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import subway.Station;
 import subway.StationRepository;
+import subway.exceptions.SubwayException;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static subway.exceptions.errors.SubwayError.NOT_FOUND;
 
 @Transactional(readOnly = true)
 @Service
@@ -34,12 +37,12 @@ public class LineService {
   }
 
   public LineResponse getLine(Long id) {
-    return LineResponse.from(lineRepository.findById(id).orElseThrow(RuntimeException::new));
+    return LineResponse.from(lineRepository.findById(id).orElseThrow(() -> new SubwayException(NOT_FOUND)));
   }
 
   @Transactional
   public LineResponse modifyLine(Long id, LineModifyRequest lineModifyRequest) {
-    Line line = lineRepository.findById(id).orElseThrow(RuntimeException::new);
+    Line line = getLineById(id);
     line.updateName(lineModifyRequest.getName());
     line.updateColor(lineModifyRequest.getColor());
     return LineResponse.from(line);
@@ -51,9 +54,13 @@ public class LineService {
   }
 
   private StationsAtLine getStationsAtLine(Long upStationId, Long downStationId) {
-    Station upStation = stationRepository.findById(upStationId).orElseThrow(RuntimeException::new);
-    Station downStation = stationRepository.findById(downStationId).orElseThrow(RuntimeException::new);
+    Station upStation = stationRepository.findById(upStationId).orElseThrow(()-> new SubwayException(NOT_FOUND));
+    Station downStation = stationRepository.findById(downStationId).orElseThrow(()-> new SubwayException(NOT_FOUND));
 
     return new StationsAtLine(upStation, downStation);
+  }
+
+  private Line getLineById(Long id) {
+    return lineRepository.findById(id).orElseThrow(()-> new SubwayException(NOT_FOUND));
   }
 }
